b50974e8e3f664ab8739f1e83f5d242441c4688e
YuliiaArtemuk <uliartemuk@gmail.com>
2023-10-08 01:08:17 +0300

    Add existing file

---
+++ main.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+fs.readFile("data.json", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  try {
+    const exchangeRates = JSON.parse(data);
+
+    let maxRate = exchangeRates[0].rate;
+
+    for (const currency of exchangeRates) {
+      if (currency.rate > maxRate) {
+        maxRate = currency.rate;
+      }
+    }
+
+    const outputText = `Максимальний курс: ${maxRate}`;
+
+    fs.writeFile("output.txt", outputText, "utf8", (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(outputText);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
